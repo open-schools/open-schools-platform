6309564fe737cb05cbe29fa66d3b129ea74193cf
zavarin-michael <33755295+zavarin-michael@users.noreply.github.com>
2022-04-10 14:00:26 +0500

    changed UTC to UTC+5 in celery settings

--- config/settings/celery.py
+++ config/settings/celery.py
@@ -5,7 +5,7 @@ from config.env import env
 CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='amqp://guest:guest@localhost//')
 CELERY_RESULT_BACKEND = 'django-db'
 
-CELERY_TIMEZONE = 'UTC'
+CELERY_TIMEZONE = 'UTC+5'
 
 CELERY_TASK_SOFT_TIME_LIMIT = 20  # seconds
 CELERT_TASK_TIME_LIMIT = 30  # seconds
