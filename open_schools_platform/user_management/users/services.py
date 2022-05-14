a8fbf3df0d0bc72de51ea973682eb1481d0f3f6f
Михаил <miha061103@gmail.com>
2022-05-14 22:34:36 +0500

    some refactoring

--- open_schools_platform/user_management/users/services.py
+++ open_schools_platform/user_management/users/services.py
@@ -1,4 +1,3 @@
-import requests
 from django.contrib.auth import authenticate
 from django.db import transaction
 from rest_framework import serializers
@@ -6,7 +5,6 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework_jwt.utils import unix_epoch
 
 from open_schools_platform.common.services import model_update
-from open_schools_platform.common.utils import get_dict_from_response
 from open_schools_platform.user_management.users.constants import RegistrationConstants
 
 from open_schools_platform.user_management.users.models import User, CreationToken
@@ -17,42 +15,12 @@ def is_token_alive(token: CreationToken) -> bool:
     return (datetime.now(timezone.utc) - token.created_at) < RegistrationConstants.LIVE_TIME
 
 
-def create_token(phone: str, recaptcha: str) -> CreationToken:
-    response = send_sms(phone, recaptcha)
-
-    if response.status_code == 200:
-        token = CreationToken.objects.create_token(
-            phone=phone,
-            session=get_dict_from_response(response)["sessionInfo"],
-        )
-        return token
-
-    return None  # type: ignore
-
-
-def send_sms(phone: str, recaptcha: str):
-    base_url = RegistrationConstants.FIREBASE_URL_TO_GET_SESSION + \
-               str(RegistrationConstants.GOOGLE_API_KEY)
-
-    request_dict = {
-        "phoneNumber": phone,
-        "recaptchaToken": recaptcha,
-    }
-
-    response = requests.post(base_url, headers={'Content-Type': 'application/json'}, json=request_dict)
-    return response
-
-
-def check_otp(session: str, otp: str):
-    base_url = RegistrationConstants.FIREBASE_URL_TO_CHECK_OTP + str(RegistrationConstants.GOOGLE_API_KEY)
-
-    request_dict = {
-        "sessionInfo": session,
-        "code": otp,
-    }
-
-    response = requests.post(base_url, headers={'Content-Type': 'application/json'}, json=request_dict)
-    return response
+def create_token(phone: str, session: str) -> CreationToken:
+    token = CreationToken.objects.create_token(
+        phone=phone,
+        session=session,
+    )
+    return token
 
 
 def create_user(phone: str, password: str, name: str, is_active: bool = True, is_admin: bool = False) -> User:
@@ -83,7 +51,7 @@ def user_update(*, user: User, data) -> User:
     return user
 
 
-def verify_token(token: CreationToken):
+def verify_token(token: CreationToken) -> CreationToken:
     token.is_verified = True
     token.save()
     return token
@@ -112,6 +80,7 @@ def get_jwt_token(username_field: str, username: str, password: str, request=Non
     return str(response_data["token"])
 
 
-def update_token_session(token: CreationToken, new_session: str):
+def update_token_session(token: CreationToken, new_session: str) -> CreationToken:
     token.session = new_session
     token.save()
+    return token
