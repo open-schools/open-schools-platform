aac853b9dc81fd18d3222447e8be890da88cfa72
Ivan <vellivanya@yandex.ru>
2022-06-16 00:20:52 +0500

    Improved handlers for user editing

--- open_schools_platform/user_management/users/urls.py
+++ open_schools_platform/user_management/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import UserApi, CreationTokenApi, VerificationApi, CodeResendApi, RetrieveCreationTokenApi, UserUpdateApi, \
-    UpdatePasswordApi, UserResetPasswordApi
+from .views import UserApi, CreationTokenApi, VerificationApi, CodeResendApi, RetrieveCreationTokenApi, \
+    UserResetPasswordApi
 
 urlpatterns = [
     path('token', CreationTokenApi.as_view(), name='create-token'),
@@ -9,7 +9,5 @@ urlpatterns = [
     path('token/<uuid:pk>/verify', VerificationApi.as_view(), name='verification-phone-by-token'),
     path('', UserApi.as_view(), name='user'),
     path('token/<uuid:pk>/resend', CodeResendApi.as_view(), name='resend'),
-    path('<uuid:pk>', UserUpdateApi.as_view(), name='user-update'),
-    path('<uuid:pk>/change-password', UpdatePasswordApi.as_view(), name='change-password'),
     path('reset-password', UserResetPasswordApi.as_view(), name='reset-password')
 ]
