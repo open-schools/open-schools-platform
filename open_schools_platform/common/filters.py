4d684f28800390b5e55f324452a919c9ec64fa9e
Михаил <miha061103@gmail.com>
2022-06-18 20:41:50 +0500

    docs for BaseFilterSet

--- open_schools_platform/common/filters.py
+++ open_schools_platform/common/filters.py
@@ -3,10 +3,24 @@ from django.db.models import Q
 from django_filters import CharFilter
 from rest_framework.exceptions import ValidationError
 
-OR_SEARCH_FIELD = "search"
-
 
 class BaseFilterSet(django_filters.FilterSet):
+    """
+    BaseFilterSet provide you some useful features:
+
+    1. Opportunity to use special field OR_SEARCH_FIELD that will search
+    for all char fields and combine results
+        * This feature works only if your input dictionary has  pair [OR_SEARCH_FIELD: some_value]
+    2. Will raise ValidationError when filter get not valid data
+    3. Will order result by "-created_at" field
+        * To use this class your input model type should inherit BaseModel
+        otherwise you can redefine ORDER_DATE_TIME
+        * To disable this feature write ORDER_DATE_TIME=None
+        * Note: symbol '-' is the reverse trigger
+    """
+    OR_SEARCH_FIELD = "search"
+    ORDER_DATE_TIME = "-created_at"
+
     def __init__(self, *args, **kwargs):
         self.search_value = None
         super().__init__(*args, **kwargs)
@@ -14,12 +28,17 @@ class BaseFilterSet(django_filters.FilterSet):
             raise ValidationError(self.errors)
 
     def OR(self, queryset, field_name, value):
+        if type(value) is not str:
+            raise ValidationError(detail="Search field must be str type.")
+
         self.search_value = value
         return queryset
 
     @property
     def qs(self):
         base_queryset = super().qs
+        if self.ORDER_DATE_TIME:
+            base_queryset = base_queryset.order_by(self.ORDER_DATE_TIME)
 
         if not self.search_value:
             return base_queryset
@@ -27,7 +46,7 @@ class BaseFilterSet(django_filters.FilterSet):
         query = Q()
         filters = self.get_filters()
         for filter in filters.values():
-            if type(filter) is CharFilter and filter.field_name != OR_SEARCH_FIELD:
+            if type(filter) is CharFilter and filter.field_name != self.OR_SEARCH_FIELD:
                 query |= Q(**{"{0}__icontains".format(filter.field_name): self.search_value})
 
         return base_queryset.filter(query)
