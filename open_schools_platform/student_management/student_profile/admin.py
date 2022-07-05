e1acf94d18171baafb28b77726a3cb6e2f3de956
ivan_gorbunov <vellivanya@yandex.ru>
2022-07-05 21:05:37 +0500

    Fixes

--- open_schools_platform/student_management/student_profile/admin.py
+++ open_schools_platform/student_management/student_profile/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from open_schools_platform.student_management.student_profile.models import StudentProfile
 
+
 @admin.register(StudentProfile)
 class StudentProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'name', 'age')
