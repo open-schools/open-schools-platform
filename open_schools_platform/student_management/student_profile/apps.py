70d1aeafeac81b27174712835b97ece2ba4dc048
ivan_gorbunov <vellivanya@yandex.ru>
2022-07-06 23:31:14 +0500

    Deleted student_profile files

--- open_schools_platform/student_management/student_profile/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StudentProfileConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'open_schools_platform.student_management.student_profile'
