1245831741596cdefeaab600ee345b2b96d2806e
ivan_gorbunov <vellivanya@yandex.ru>
2022-07-13 00:05:07 +0500

    Fixed urls

--- open_schools_platform/student_management/student/urls.py
+++ open_schools_platform/student_management/student/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from open_schools_platform.student_management.student.views import StudentProfileApi, StudentJoinCircleApi
 
 urlpatterns = [
-    path('student-profile', StudentProfileApi.as_view(), name='create_student_profile'),
+    path('student-profile', StudentProfileApi.as_view(), name='create-student-profile'),
     path('<uuid:pk>/student-join-circle', StudentJoinCircleApi.as_view(),
-         name='student_join_circle')
+         name='student-join-circle')
 ]
