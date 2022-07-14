3315b1d3997d9328f1afdf8f88d8cefbc68bbbf5
ivan_gorbunov <vellivanya@yandex.ru>
2022-07-14 13:47:14 +0500

    Improved CreateCircleTests

--- open_schools_platform/organization_management/circles/tests/services/test_circle_services.py
+++ open_schools_platform/organization_management/circles/tests/services/test_circle_services.py
@@ -1,12 +1,10 @@
 from django.test import TestCase
 
 from open_schools_platform.organization_management.circles.models import Circle
-from open_schools_platform.organization_management.circles.services import create_circle
-from open_schools_platform.organization_management.organizations.services import create_organization
+from open_schools_platform.organization_management.circles.tests.utils.test_utils import create_test_circle
 
 
 class CreateCircleTests(TestCase):
     def test_successful_circle_creation(self):
-        organization = create_organization(name="test_org", inn="1111111111")
-        create_circle(name="test_circle", organization=organization)
+        create_test_circle()
         self.assertEqual(1, Circle.objects.count())
