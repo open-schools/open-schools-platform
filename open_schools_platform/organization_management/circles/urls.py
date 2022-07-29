64b0bdec0ab9702ce078d1d16127d482eab76d2c
ivan_gorbunov <vellivanya@yandex.ru>
2022-07-29 14:21:35 +0500

    Improved response messages and added new api handlers

--- open_schools_platform/organization_management/circles/urls.py
+++ open_schools_platform/organization_management/circles/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from open_schools_platform.common.views import MultipleViewManager
-from open_schools_platform.organization_management.circles.views import CreateCircleApi, GetCircles
+from open_schools_platform.organization_management.circles.views import CreateCircleApi, GetCircles, \
+    CirclesQueriesListApi
 
 urlpatterns = [
     path('', MultipleViewManager({'post': CreateCircleApi, 'get': GetCircles}).as_view(), name='circles'),
+    path('<uuid:pk>/queries', CirclesQueriesListApi.as_view(), name='queries-list')
 ]
