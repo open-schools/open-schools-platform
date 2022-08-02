4c2652ca249e8f860fe56ee2a5a8f9adc75bc729
ivan_gorbunov <vellivanya@yandex.ru>
2022-08-02 21:25:16 +0500

    Improved test utils

--- open_schools_platform/organization_management/circles/tests/utils.py
+++ open_schools_platform/organization_management/circles/tests/utils.py
@@ -1,6 +1,6 @@
 from open_schools_platform.organization_management.circles.models import Circle
 from open_schools_platform.organization_management.circles.services import create_circle
-from open_schools_platform.organization_management.employees.tests.utils import create_test_employee
+from open_schools_platform.organization_management.employees.services import create_employee
 from open_schools_platform.organization_management.organizations.services import create_organization
 from open_schools_platform.user_management.users.models import User
 
@@ -13,7 +13,8 @@ def create_test_circle() -> Circle:
 
 def create_test_circle_with_user_in_org(user: User) -> Circle:
     organization = create_organization(name="test_org", inn="1111111111")
-    employee = create_test_employee(user=user)
-    organization.employees.add(employee)
+    employee = create_employee(name="test_employee", position="test", user=user)
+    employee.organization = organization
+    employee.save()
     circle = create_circle(organization=organization, name="test_circle", address="d", capacity=0, description="alalal")
     return circle
