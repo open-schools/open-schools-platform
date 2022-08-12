d4a6b1c0810f649e8cf4485d83a33878c0ea4563
ivan_gorbunov <vellivanya@yandex.ru>
2022-08-12 17:08:01 +0500

    Improved Query filtering

--- open_schools_platform/query_management/queries/admin.py
+++ open_schools_platform/query_management/queries/admin.py
@@ -6,9 +6,9 @@ from open_schools_platform.query_management.queries.services import run_sender_h
 from django.utils.translation import gettext_lazy as _
 
 
-class SenderFilter(InputFilter):
-    parameter_name = 'sender'
-    title = _('sender')
+class SenderCTFilter(InputFilter):
+    parameter_name = 'sender_ct_model'
+    title = _('sender CT model')
 
     def queryset(self, request, queryset):
         if self.value() is not None:
@@ -17,9 +17,9 @@ class SenderFilter(InputFilter):
             return get_queries(filters={"sender_ct_search": sender})
 
 
-class RecipientFilter(InputFilter):
-    parameter_name = 'recipient'
-    title = _('recipient')
+class RecipientCTFilter(InputFilter):
+    parameter_name = 'recipient_ct_model'
+    title = _('recipient CT model')
 
     def queryset(self, request, queryset):
         if self.value() is not None:
@@ -28,9 +28,31 @@ class RecipientFilter(InputFilter):
             return get_queries(filters={"recipient_ct_search": recipient})
 
 
+class SenderUUIDFilter(InputFilter):
+    parameter_name = 'sender_uuid'
+    title = _('sender UUID')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            sender_uuid = self.value()
+
+            return get_queries(filters={'sender_id': sender_uuid})
+
+
+class RecipientUUIDFilter(InputFilter):
+    parameter_name = 'recipient_uuid'
+    title = _('recipient UUID')
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            recipient_uuid = self.value()
+
+            return get_queries(filters={'recipient_id': recipient_uuid})
+
+
 class QueryAdmin(admin.ModelAdmin):
-    list_display = ("id", "sender_ct", "recipient_ct", "status")
-    list_filter = ("status", SenderFilter, RecipientFilter)
+    list_display = ("id", "sender_ct", "recipient_ct", "status", "created_at")
+    list_filter = ("status", SenderCTFilter, SenderUUIDFilter, RecipientCTFilter, RecipientUUIDFilter)
 
     def save_model(self, request, obj, form, change):
         old_query = get_query(filters={"id": str(obj.id)})
