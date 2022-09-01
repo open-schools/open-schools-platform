5de76ec8931e9a4b1c8c8a0e19ea137cdc5313fc
ivan_gorbunov <vellivanya@yandex.ru>
2022-09-01 17:37:53 +0500

    Added migration for correcting location field of already created circles

---
+++ open_schools_platform/organization_management/circles/migrations/0004_reverse_location.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+
+def reverse_circles_location(apps, schema_editor):
+    circles = apps.get_model('circles', 'Circle')
+    for circle in circles.objects.all():
+        x = circle.location.x
+        circle.location.x = circle.location.y
+        circle.location.y = x
+        circle.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('circles', '0003_circle_location'),
+    ]
+
+    operations = [
+        migrations.RunPython(reverse_circles_location)
+    ]
