c2ddb41b88905875480fb6fccf941dbc5bf16252
timofeus <byz0vtm@gmail.com>
2022-09-13 19:32:08 +0500

    fix: remove heartbeat flag + comment

--- open_schools_platform/organization_management/organizations/tests/emails/send_example.py
+++ open_schools_platform/organization_management/organizations/tests/emails/send_example.py
@@ -5,6 +5,7 @@ from open_schools_platform.tasks.tasks import send_mail_to_new_user_with_celery
 
 class TestSender(TestCase):
 
+    # run for debug sending mail
     def test_SDK_send_with_celery(self):
         send_mail_to_new_user_with_celery.delay("TEST",
                                                 {'login': 'TEST_LOGIN', 'password': 'TEST_PASSWORD',
