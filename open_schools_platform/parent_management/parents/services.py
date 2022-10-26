c5e36e1845113d9efa4d0e3d27068c9a6f0ca3a4
ElizavetaPetrova <elizavetapet@bk.ru>
2022-10-26 19:29:16 +0500

    add template for circle invitation email

--- open_schools_platform/parent_management/parents/services.py
+++ open_schools_platform/parent_management/parents/services.py
@@ -1,9 +1,11 @@
+from open_schools_platform.common.constants import CommonConstants
 from open_schools_platform.parent_management.families.selectors import get_family
 from open_schools_platform.parent_management.families.services import create_family
 from open_schools_platform.parent_management.parents.models import ParentProfile
 
 from open_schools_platform.user_management.users.selectors import get_user
 from open_schools_platform.user_management.users.services import generate_user_password, create_user
+from open_schools_platform.tasks.tasks import send_mail_to_new_user_with_celery
 
 
 def get_parent_profile_or_create_new_user(phone: str, email: str, circle_name: str) -> ParentProfile:
@@ -11,12 +13,12 @@ def get_parent_profile_or_create_new_user(phone: str, email: str, circle_name: s
 
     if not user:
         pwd = generate_user_password()
-        name = 'Родитель 1'
-        # send_mail_to_new_user_with_celery.delay('Приглашение в кружок',
-        #                                         {'login': phone, 'password': pwd, 'circle': circle_name,
-        #                                         'name': name},
-        #                                         CommonConstants.DEFAULT_FROM_EMAIL,
-        #                                         email)
+        name = 'Родитель'
+        send_mail_to_new_user_with_celery.delay('Приглашение в кружок',
+                                                {'login': phone, 'password': pwd, 'circle': circle_name,
+                                                 'name': name},
+                                                CommonConstants.DEFAULT_FROM_EMAIL,
+                                                email, 'new_user_circle_invite_mail_form.html')
         user = create_user(phone=phone, password=pwd, name=name, email=email)
 
     return user.parent_profile
--- open_schools_platform/tasks/tasks.py
+++ open_schools_platform/tasks/tasks.py
@@ -6,7 +6,7 @@ from open_schools_platform.common.constants import CommonConstants
 
 
 @app.task
-def send_mail_to_new_user_with_celery(subject, html_kwargs, from_email, to):
+def send_mail_to_new_user_with_celery(subject, html_kwargs, from_email, to, template='new_user_invite_mail_form.html'):
     sdk = SendBoxApi(CommonConstants.EMAIL_ID, CommonConstants.EMAIL_PRIVATE_API_KEY)
-    html = render_to_string('new_user_invite_mail_form.html', html_kwargs)
+    html = render_to_string(template, html_kwargs)
     sdk.send_html_email(subject, from_email, 'Открытые Школы', to, 'Уважаемый пользователь', html, '')
