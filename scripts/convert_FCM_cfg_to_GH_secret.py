4055f61fc290ee5a180e5185d859b931a33c1a44
ivan_gorbunov <vellivanya@yandex.ru>
2022-11-06 19:20:48 +0500

    Added script for converting config to secret

---
+++ scripts/convert_FCM_cfg_to_GH_secret.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from pathlib import Path
+
+
+def convert_config_to_secret(config):
+    s = ""
+    for i in config:
+        s += i.replace("\n", "").replace("\"", "\\\"").replace("{", "\\{").replace("}", "\\}"). \
+            replace("\\n", "\\\\n")
+
+    return s
+
+BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__))).parent.absolute()
+
+frame = sys._getframe()
+
+FIREBASE_ADMIN_CONFIG = os.path.join(BASE_DIR, '.firebase_admin_config')
+
+print(convert_config_to_secret(open(FIREBASE_ADMIN_CONFIG).readlines()))
