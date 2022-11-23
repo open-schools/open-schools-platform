b86f5da95b6104f652cca324ab312d972c684c6d
Feazom <bayastanov1an@gmail.com>
2022-11-23 21:46:12 +0500

    removed hard delete for soft models in admin panel

--- open_schools_platform/common/admin.py
+++ open_schools_platform/common/admin.py
@@ -1,10 +1,7 @@
 from typing import Type, Sequence, Union, Callable, Any, Tuple
 
-from django.contrib import admin, messages
+from django.contrib import admin
 from django.contrib.admin import register
-from django.contrib.admin.utils import model_ngettext
-from django.core.exceptions import PermissionDenied
-from django.utils.encoding import force_str
 from safedelete import HARD_DELETE, SOFT_DELETE, SOFT_DELETE_CASCADE, NO_DELETE
 from safedelete.admin import SafeDeleteAdmin, SafeDeleteAdminFilter
 
@@ -16,43 +13,6 @@ class BaseAdmin(SafeDeleteAdmin):
     list_filter = (SafeDeleteAdminFilter,) + SafeDeleteAdmin.list_filter  # type: ignore[operator]
     actions: Tuple = ()
     field_to_highlight = "name"
-
-    @admin.action(description='Hard deleted selected')
-    def hard_delete_selected(self, request, queryset):
-        """ Admin action to hard delete objects in bulk with confirmation. """
-        if not self.has_delete_permission(request):
-            raise PermissionDenied
-        assert hasattr(queryset, 'delete')
-
-        requested = queryset.count()
-        changed = 0
-        if requested:
-            for obj in queryset:
-                obj_display = force_str(obj)
-                self.log_deletion(request, obj, obj_display)
-            df = queryset.delete(force_policy=HARD_DELETE)
-            changed += df[1]['users.User']
-            if changed < requested:
-                self.message_user(
-                    request,
-                    "Successfully deleted %(count_changed)d of the "
-                    "%(count_requested)d selected %(items)s." % {
-                        "count_requested": requested,
-                        "count_changed": changed,
-                        "items": model_ngettext(self.opts, requested)
-                    },
-                    messages.WARNING,
-                )
-            else:
-                self.message_user(
-                    request,
-                    "Successfully deleted %(count)d %(items)s." % {
-                        "count": requested,
-                        "items": model_ngettext(self.opts, requested)
-                    },
-                    messages.SUCCESS,
-                )
-            return None
 
     class Meta:
         abstract = True
@@ -83,7 +43,7 @@ def admin_wrapper(model: Type[BaseModel]):
         delete_policy = model._safedelete_policy
         if delete_policy:
             if delete_policy == SOFT_DELETE or delete_policy == SOFT_DELETE_CASCADE:
-                admin_model.actions = ('hard_delete_selected', 'undelete_selected')
+                admin_model.actions = ('undelete_selected',)
                 admin_model.highlight_deleted_field.short_description = admin_model.field_to_highlight  # type: ignore[attr-defined] # noqa
 
                 if admin_model.list_display:
