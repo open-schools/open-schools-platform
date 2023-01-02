a9303500698567e8029d564cf08b6aa7a556af5a
Feazom <bayastanov1an@gmail.com>
2023-01-02 20:44:31 +0500

    added history for teachers

---
+++ open_schools_platform/history_management/serializers/teacher_serializer.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from open_schools_platform.history_management.serializers.fields import HistorySerializerFields
+from open_schools_platform.history_management.swagger_schemas_generator import SwaggerSchemasHistoryGenerator
+from open_schools_platform.organization_management.teachers.models import Teacher, TeacherProfile
+
+
+class TeacherHistorySerializer(serializers.ModelSerializer):
+    history = HistorySerializerFields.get_history_records_field(
+        fields=HistorySerializerFields().HISTORY_TEACHER_FIELDS)(read_only=True)
+
+    class Meta:
+        model = Teacher
+        fields = ("history",)
+        swagger_schema_fields = SwaggerSchemasHistoryGenerator(fields=HistorySerializerFields().HISTORY_TEACHER_FIELDS,
+                                                               object_title='TeacherHistory',
+                                                               model=Teacher).generate_schemas()
+
+
+class TeacherProfileHistorySerializer(serializers.ModelSerializer):
+    history = HistorySerializerFields.get_history_records_field(
+        fields=HistorySerializerFields().HISTORY_TEACHER_PROFILES_FIELDS)(read_only=True)
+
+    class Meta:
+        model = TeacherProfile
+        fields = ("history",)
+        swagger_schema_fields = SwaggerSchemasHistoryGenerator(
+            fields=HistorySerializerFields().HISTORY_TEACHER_PROFILES_FIELDS,
+            object_title='TeacherProfileHistory',
+            model=TeacherProfile).generate_schemas()
