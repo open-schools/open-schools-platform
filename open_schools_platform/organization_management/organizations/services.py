3b104d058bfc4df1604ae5082fded2f7c7f8e04e
Feazom <bayastanov1an@gmail.com>
2023-01-22 15:59:19 +0500

    merge from dev

--- open_schools_platform/organization_management/organizations/services.py
+++ open_schools_platform/organization_management/organizations/services.py
@@ -1,5 +1,4 @@
 from django.db.models import QuerySet
-from rest_framework.exceptions import ValidationError, MethodNotAllowed, NotAcceptable
 import typing
 
 from rest_framework.exceptions import MethodNotAllowed
