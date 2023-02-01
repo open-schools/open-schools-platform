7f6835b6a3acf04d8b47fe320eaa6fa2aa6f8fed
Feazom <bayastanov1an@gmail.com>
2023-02-01 19:56:35 +0500

    added far circles suggestion

--- open_schools_platform/organization_management/circles/filters.py
+++ open_schools_platform/organization_management/circles/filters.py
@@ -1,21 +1,34 @@
 from django.contrib.gis.measure import D
-from django_filters import CharFilter
+from django_filters import CharFilter, BooleanFilter
 
 from open_schools_platform.common.filters import BaseFilterSet, filter_by_ids
 from open_schools_platform.organization_management.circles.constants import CirclesConstants
 from open_schools_platform.organization_management.circles.models import Circle
 
 
-def circle_radius_filter(queryset, name, value):
-    return queryset.filter(location__distance_lte=(value, D(km=CirclesConstants.SEARCH_RADIUS)))
-
-
 class CircleFilter(BaseFilterSet):
     ids = CharFilter(method=filter_by_ids)
     address = CharFilter(field_name="address", lookup_expr="icontains")
     organization_name = CharFilter(field_name="organization__name", lookup_expr="icontains")
-    user_location = CharFilter(method=circle_radius_filter)
+    suggest_far_circles = BooleanFilter(method='circle_suggestions_filter')
+    user_location = CharFilter(method='circle_radius_filter')
     name = CharFilter(field_name="name", lookup_expr="icontains")
+    suggest = False
+
+    def circle_suggestions_filter(self, queryset, name, value):
+        self.suggest = value
+        return queryset
+
+    def circle_radius_filter(self, queryset, name, value):
+        radius = CirclesConstants.SEARCH_RADIUS
+        result = queryset.filter(location__distance_lte=(value, D(km=radius)))
+        if self.suggest and len(result) == 0:
+            for i in range(3):
+                radius *= 10
+                result = queryset.filter(location__distance_lte=(value, D(km=radius)))
+                if len(result) > 0:
+                    return result
+        return result
 
     class Meta:
         model = Circle
