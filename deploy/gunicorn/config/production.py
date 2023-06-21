b7243b32445b09f0e3b7b1ad55558bb082c52056
zavarin-michael <miha061103@gmail.com>
2023-06-21 18:54:19 +0500

    .gitignore for .env.db

--- deploy/gunicorn/config/production.py
+++ deploy/gunicorn/config/production.py
@@ -9,11 +9,11 @@ workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
 bind = "0.0.0.0:8000"
 # Write access and error info to /var/log
-accesslog = "/app/logs/gunicorn/access.log"
-errorlog = "/app/logs/gunicorn/error.log"
+accesslog = "/app/logs/gunicorn-access.log"
+errorlog = "/app/logs/gunicorn-error.log"
 # Redirect stdout/stderr to log file
 capture_output = True
 # PID file so you can easily fetch process ID
-pidfile = "/app/logs/gunicorn/prod.pid"
+pidfile = "/app/logs/gunicorn-prod.pid"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = False
