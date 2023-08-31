6c34d5941cd9997b602f9c9ce93985982d0959b6
Михаил <miha061103@gmail.com>
2023-08-31 16:00:55 +0500

    new request fro certain circle in organization

--- open_schools_platform/organization_management/circles/selectors.py
+++ open_schools_platform/organization_management/circles/selectors.py
@@ -24,7 +24,7 @@ def get_circles(*, filters=None, prefetch_related_list=None) -> QuerySet:
 def get_circle(*, filters=None, user: User = None, prefetch_related_list=None) -> Circle:
     filters = filters or {}
 
-    qs = Circle.objects.all()
+    qs = Circle.objects.prefetch_related(*prefetch_related_list).all()
     circle = CircleFilter(filters, qs).qs.first()
 
     if user and circle and not user.has_perm("circles.circle_access", circle):
