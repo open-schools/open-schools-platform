cd02167c2a2f2cc88b611080c0f404f62b11bf40
nepoal <sarkisyan19752005@gmail.com>
2025-12-16 15:59:18 +0500

    add sort filter for /apps/

--- open_schools_platform/marketplace_management/filters.py
+++ open_schools_platform/marketplace_management/filters.py
@@ -5,6 +5,14 @@ from open_schools_platform.marketplace_management.models import App
 
 
 class AppFilterset(filters.FilterSet):
+    SORT_CHOICES = (
+        ("newest", "Newest"),
+        # ("rating", "Rating"),
+        # ("popular", "Popular"),
+        ("relevance", "Relevance"),
+    )
+
+    sort = filters.ChoiceFilter(method="filter_sort", choices=SORT_CHOICES)
     category_id = filters.NumberFilter("category_id")
     type = filters.ChoiceFilter(field_name="type", choices=App.APP_TYPES)
     q = filters.CharFilter(method="filter_q")
@@ -13,6 +21,14 @@ class AppFilterset(filters.FilterSet):
         return queryset.filter(
             Q(name__icontains=value) | Q(description__icontains=value)
         )
+
+    def filter_sort(self, queryset, name, value):
+        if value == "newest":
+            return queryset.order_by("-updated_at")
+        elif value == "relevance":
+            return queryset.order_by("-updated_at")
+        else:
+            return queryset
 
     class Meta:
         model = App
