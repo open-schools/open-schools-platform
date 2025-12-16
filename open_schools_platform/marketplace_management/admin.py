6a7e49b25763cc8b53550d275b9314cf88f19469
nepoal <sarkisyan19752005@gmail.com>
2025-12-16 23:58:45 +0500

    add models in admin

--- open_schools_platform/marketplace_management/admin.py
+++ open_schools_platform/marketplace_management/admin.py
@@ -1,2 +1,38 @@
+from open_schools_platform.common.admin import admin_wrapper, BaseAdmin
+from open_schools_platform.marketplace_management.models import Installation, DeveloperProfile, Category, App, \
+    AppRelease, Review
 
-# Register your models here.
+
+@admin_wrapper(DeveloperProfile)
+class DeveloperProfileModelAdmin(BaseAdmin):
+    list_display = ("id", "user", "email", "github")
+    field_to_highlight = "id"
+
+
+@admin_wrapper(Category)
+class CategoryModelAdmin(BaseAdmin):
+    list_display = ("id", "name")
+
+
+@admin_wrapper(App)
+class AppModelAdmin(BaseAdmin):
+    list_display = ("id", "name", "status", "developer_profile")
+
+
+@admin_wrapper(AppRelease)
+class AppReleaseModelAdmin(BaseAdmin):
+    list_display = ("id", "app", "version")
+    field_to_highlight = "id"
+
+
+@admin_wrapper(Review)
+class ReviewModelAdmin(BaseAdmin):
+    list_display = ("id", "user", "app", "rating")
+    field_to_highlight = "app"
+
+
+@admin_wrapper(Installation)
+class InstallationModelAdmin(BaseAdmin):
+    list_display = ("id", "app", "active")
+    field_to_highlight = "app"
+
