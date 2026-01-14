d3df1ffb5b5beef99ac36223bed3f1e12547f151
filippov-da <dfilippov@runity.ru>
2026-01-14 13:59:13 +0500

    Try jsonschema older version

--- config/settings/push_notifications.py
+++ config/settings/push_notifications.py
@@ -1,20 +1,22 @@
-import firebase_admin
-from firebase_admin import credentials
-from ..env import env
+# import firebase_admin
+# from firebase_admin import credentials
+# from ..env import env
+#
+# cred = credentials.Certificate(
+#     {
+#         "type": "service_account",
+#         "project_id": env("FIREBASE_PROJECT_ID", default=""),
+#         "private_key_id": env("FIREBASE_PRIVATE_KEY_ID", default=""),
+#         "private_key": env("FIREBASE_PRIVATE_KEY", default="").replace("\\n", "\n"),
+#         "client_email": env("FIREBASE_CLIENT_EMAIL", default=""),
+#         "client_id": env("FIREBASE_CLIENT_ID", default=""),
+#         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#         "token_uri": "https://accounts.google.com/o/oauth2/token",
+#         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#         "client_x509_cert_url": env("FIREBASE_CLIENT_CERT_URL", default=""),
+#     }
+# )
+#
+# app = firebase_admin.initialize_app(cred)
 
-cred = credentials.Certificate(
-    {
-        "type": "service_account",
-        "project_id": env("FIREBASE_PROJECT_ID", default=""),
-        "private_key_id": env("FIREBASE_PRIVATE_KEY_ID", default=""),
-        "private_key": env("FIREBASE_PRIVATE_KEY", default="").replace("\\n", "\n"),
-        "client_email": env("FIREBASE_CLIENT_EMAIL", default=""),
-        "client_id": env("FIREBASE_CLIENT_ID", default=""),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://accounts.google.com/o/oauth2/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": env("FIREBASE_CLIENT_CERT_URL", default=""),
-    }
-)
-
-app = firebase_admin.initialize_app(cred)
+app = None
