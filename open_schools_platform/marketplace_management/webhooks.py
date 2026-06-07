877bb473be5936ea4c40a7f07b27a3e78685b7e8
an.imag <a.e.grekhov@gmail.com>
2026-06-07 15:33:44 +0500

    fix: исправление загрузки iframe (app_url) и ReferenceError в модалке

--- open_schools_platform/marketplace_management/webhooks.py
+++ open_schools_platform/marketplace_management/webhooks.py
@@ -17,6 +17,7 @@ class JiraWebhookSerializer(serializers.Serializer):
     description = serializers.CharField()
     ticket_id = serializers.CharField(required=False, allow_blank=True, default="")
     icon_url = serializers.URLField(required=False, allow_blank=True, default="")
+    app_url = serializers.URLField(required=False, allow_blank=True, default="")
     redirect_uris = serializers.ListField(child=serializers.URLField(), required=False, default=list)
     required_scopes = serializers.ListField(child=serializers.CharField(), required=False, default=list)
     optional_scopes = serializers.ListField(child=serializers.CharField(), required=False, default=list)
@@ -61,6 +62,7 @@ class JiraApproveWebhookView(APIView):
             name=data["name"],
             description=data["description"],
             icon_url=data.get("icon_url", ""),
+            app_url=data.get("app_url", ""),
             redirect_uris=data.get("redirect_uris", []),
             required_scopes=data.get("required_scopes", []),
             optional_scopes=data.get("optional_scopes", []),
